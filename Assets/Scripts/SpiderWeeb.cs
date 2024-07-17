e50d507d1e85871e3a583f505ab810faaa53aca2
Marco Antonio Diaz Valdes <44275048+Marco1969theKnight@users.noreply.github.com>
2024-07-17 13:18:26 -0600

    Se agregaron profundidades, se resalto de manera visual los objetos que se pueden recoger, se invoncan barras de vida a los enemigos y se tiene uniforme el estilo de las barras de vida y estamina en todos los niveles,

---
+++ Assets/Scripts/SpiderWeeb.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterStats))]
+
+public class SpiderWeeb : MonoBehaviour
+{
+    protected CharacterStats stats;
+    private float maxS;
+    private float moveS;
+
+    public virtual void Start ()
+	{
+		stats = GetComponent<CharacterStats>();
+	}
+
+    // Update is called once per frame
+    public virtual void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            PlayerStats ps = col.transform.parent.GetComponent<PlayerStats>();
+            maxS = ps.maxSprint;
+            moveS = ps.moveSpeed;
+
+
+        }
+        
+    }
+
+    public virtual void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            PlayerStats ps = col.transform.parent.GetComponent<PlayerStats>();
+            ps.maxSprint = maxS;
+            moveS = ps.moveSpeed;
+        }
+        
+    }
+}
