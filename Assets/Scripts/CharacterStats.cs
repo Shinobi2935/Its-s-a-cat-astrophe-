c165137b29e1cfcfad3f109ecf716a736b56d246
Marco Antonio Diaz Valdes <44275048+Marco1969theKnight@users.noreply.github.com>
2024-08-16 15:45:34 -0600

    Se reintegraron los cambios hechos a lo largo del proyecto

--- Assets/Scripts/CharacterStats.cs
+++ Assets/Scripts/CharacterStats.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.InputSystem;
 
 public class CharacterStats : MonoBehaviour
@@ -8,6 +9,9 @@ public class CharacterStats : MonoBehaviour
 	public static CharacterStats Instance;
     [SerializeField] public AudioClip damageAudio;
     [SerializeField] public AudioClip deathAudio;
+    [SerializeField] private Collider2D m_Collider;
+    [SerializeField] private Slider health;
+    [SerializeField] private GameObject canvas;
 	public int maxHealth = 100;
     public int currentHealth;
 
@@ -29,6 +33,7 @@ public class CharacterStats : MonoBehaviour
     public virtual void Start ()
 	{
 		Instance = this;
+        if(health != null) health.value = 1;
     }
     //private void Update()
     //{
@@ -42,6 +47,7 @@ public class CharacterStats : MonoBehaviour
     // Damage the character
     public virtual void TakeDamage (int damage)
 	{
+        if(canvas != null) canvas.SetActive(true);
 		// Subtract the armor value - Make sure damage doesn't go below 0.
 
 		damage -= armor.GetValue();
@@ -49,8 +55,8 @@ public class CharacterStats : MonoBehaviour
 
 		// Subtract damage from health
 		currentHealth -= damage;
-		Debug.Log(transform.name + " takes " + damage + " damage.");
-        Debug.Log(damageAudio);
+        Debug.Log("health per: " + currentHealth);
+        if(health != null) health.value = (float)currentHealth/maxHealth;
         characterAudio.clip = damageAudio;
         characterAudio.Play();
         
@@ -58,6 +64,7 @@ public class CharacterStats : MonoBehaviour
 
 		if (currentHealth <= 0)
         {
+            if(m_Collider != null) m_Collider.enabled = false;
             StartCoroutine(Die());
         }
 	}
